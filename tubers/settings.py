88de883cd14aa2757712d7a32837b5015465a03a
Ramesh konagala <rameshkonagala43@gmail.com>
2021-08-13 21:52:56 +0530

    media

--- tubers/settings.py
+++ tubers/settings.py
@@ -141,10 +141,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_DIRS = [
-   # os.path.join(BASE_DIR, 'tubers/static')
-#]
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'tubers/static')
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
--- tubers/urls.py
+++ tubers/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('contactus/', include('contactus.urls')),
     path('accounts/', include('accounts.urls')),
     path('socialaccounts/', include('allauth.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
